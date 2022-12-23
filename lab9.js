1ab2e1663ca71b71213cb2c52185709fd4491618
Егорова  Алёна <aalenaegorova@mail.ru>
2022-12-24 06:30:21 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -10,3 +10,21 @@ function clickMe1(button){
     else
         button.innerHTML = 'Имя';
 }
+function clickMe2(button){
+    if( button.innerHTML == 'Нажми меня'){
+        button.innerHTML = 'Ещё раз нажми';
+        button.style.color = 'SteelBlue';
+    }
+    else if( button.innerHTML == 'Ещё раз нажми'){
+        button.innerHTML = 'Ещё';
+        button.style.color = 'Aqua';}
+    else if( button.innerHTML == 'Ещё'){
+        button.innerHTML = 'Начать сначала';
+        button.style.color = 'LightSeaGreen';}
+    else if( button.innerHTML == 'Начать сначала'){
+        button.innerHTML = 'Нажми меня';
+        button.style.color = 'Blue';}
+    else
+        button.innerHTML = 'Нажми меня';
+}
+
